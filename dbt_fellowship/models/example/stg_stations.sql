549ee67e89e0867df1a7bb51c05ff2d57072ba88
Ashwin Hareesh <ashwinhar@gmail.com>
2024-09-30 10:16:14 -0400

    staging table for stations

---
+++ dbt_fellowship/models/example/stg_stations.sql
@@ -0,0 +1,20 @@
+{{
+    config(
+        materialized='table'
+    )
+}}
+
+SELECT
+    gtfs_stop_id
+   ,station_id
+   ,complex_id
+   ,line
+   ,stop_name
+   ,borough
+   ,gtfs_latitude
+   ,gtfs_longitude
+   ,ada
+   ,ada_northbound
+   ,ada_southbound
+   ,georeference
+FROM stations
