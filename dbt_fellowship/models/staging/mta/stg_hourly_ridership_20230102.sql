51590563bf3461a8ec18ccb169cb610bea6aa533
Ashwin Hareesh <ashwinhar@gmail.com>
2024-10-01 10:42:04 -0400

    added new source and then staging table for hourly_ridership_20230102

---
+++ dbt_fellowship/models/staging/mta/stg_hourly_ridership_20230102.sql
@@ -0,0 +1,11 @@
+SELECT 
+    CAST(transit_timestamp AS DATE)     transit_timestamp
+   ,transit_mode                        transit_mode
+   ,station_complex_id                  station_complex_id
+   ,station_complex                     station_complex
+   ,payment_method                      payment_method
+   ,fare_class_category                 fare_class_category
+   ,ridership                           original_ridership
+   ,CAST(ridership AS INT)              ridership
+   ,CAST(transfers AS INT)              transfers
+FROM {{source ('mta', 'hourly_ridership_20230102' )}}
