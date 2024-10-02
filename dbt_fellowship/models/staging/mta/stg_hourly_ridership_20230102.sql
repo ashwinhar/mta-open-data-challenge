54f632c091cd0fddcb8d97347c9e0c518474d60a
Ashwin Hareesh <ashwinhar@gmail.com>
2024-10-02 14:58:04 -0400

    Fixed tabbing, edited documentation

--- dbt_fellowship/models/staging/mta/stg_hourly_ridership_20230102.sql
+++ dbt_fellowship/models/staging/mta/stg_hourly_ridership_20230102.sql
@@ -1,11 +1,10 @@
 SELECT 
     CAST(transit_timestamp AS TIMESTAMP)     transit_timestamp
-   ,transit_mode                        transit_mode
-   ,station_complex_id                  station_complex_id
-   ,station_complex                     station_complex
-   ,payment_method                      payment_method
-   ,fare_class_category                 fare_class_category
-   ,ridership                           original_ridership
-   ,CAST(ridership AS INT)              ridership
-   ,CAST(transfers AS INT)              transfers
+   ,transit_mode                             transit_mode
+   ,station_complex_id                       station_complex_id
+   ,station_complex                          station_complex
+   ,payment_method                           payment_method
+   ,fare_class_category                      fare_class_category
+   ,CAST(ridership AS INT)                   ridership
+   ,CAST(transfers AS INT)                   transfers
 FROM {{source ('mta', 'hourly_ridership_20230102' )}}
