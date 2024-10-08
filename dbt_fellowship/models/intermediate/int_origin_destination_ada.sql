33421e7a15a0c0d79e3105e3c83c22ea06e4b68e
Ashwin Hareesh <ashwinhar@gmail.com>
2024-10-08 10:27:26 -0500

    Cleaning up columns in model

--- dbt_fellowship/models/intermediate/int_origin_destination_ada.sql
+++ dbt_fellowship/models/intermediate/int_origin_destination_ada.sql
@@ -1,5 +1,5 @@
 SELECT 
-	*
+	sod.*
 FROM {{ref("stg_origin_destination")}}	                            sod
 inner join	{{ref("int_ada_complexes")}}	                        iac_o
 	on sod.origin_station_complex_id = iac_o.ada_complex_id
