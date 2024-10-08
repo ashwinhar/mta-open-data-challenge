f358c30bbd7b592baba8fc0f31f17170ee92175d
Ashwin Hareesh <ashwinhar@gmail.com>
2024-10-08 08:40:47 -0500

    new column aliases

--- dbt_fellowship/models/intermediate/int_hourly_ridership_filteraggregate.sql
+++ dbt_fellowship/models/intermediate/int_hourly_ridership_filteraggregate.sql
@@ -2,16 +2,16 @@ WITH cte AS (
 SELECT * FROM {{ref('int_hourly_ridership_timestamp_parts')}}
 )
 SELECT 
-	transit_month
+    transit_month
    ,transit_hour_of_day
    ,transit_day_of_week
-   ,station_complex_id
+   ,origin_station_complex_id
    ,avg(ridership)			average_disability_ridership
 FROM cte 
 WHERE 
 	fare_class_category IN ('Metrocard - Seniors & Disability', 'OMNY - Seniors & Disability')
 GROUP BY
-	transit_month
+    transit_month
    ,transit_hour_of_day
    ,transit_day_of_week
-   ,station_complex_id
+   ,origin_station_complex_id
--- dbt_fellowship/models/staging/mta/stg_hourly_ridership.sql
+++ dbt_fellowship/models/staging/mta/stg_hourly_ridership.sql
@@ -1,8 +1,8 @@
 SELECT 
     CAST(transit_timestamp AS TIMESTAMP)     transit_timestamp
    ,transit_mode                             transit_mode
-   ,station_complex_id                       station_complex_id
-   ,station_complex                          station_complex
+   ,station_complex_id                       origin_station_complex_id
+   ,station_complex                          origin_station_complex_name
    ,payment_method                           payment_method
    ,fare_class_category                      fare_class_category
    ,CAST(ridership AS INT)                   ridership
