bb097c5abcf0f94c4ab991bda2b57fb069ad86ae
Ashwin Hareesh <ashwinhar@gmail.com>
2024-10-10 16:41:03 -0500

    new models

--- dbt_fellowship/models/intermediate/int_all_complexes.sql
+++ dbt_fellowship/models/intermediate/int_all_complexes.sql
@@ -1,9 +1,6 @@
 SELECT 
     complex_id						ada_complex_id
-   ,LIST(DISTINCT stop_name)		stop_name_list
-   ,LIST(line)						line_list
    ,FIRST(gtfs_latitude)            gtfs_latitude
    ,FIRST(gtfs_longitude)           gtfs_longitude
 FROM {{ref("stg_stations")}}
-WHERE ada = 1
 GROUP BY complex_id
