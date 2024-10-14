3dd42c28fc2d92d8560feec4841c2d25325aa030
Ashwin Hareesh <ashwinhar@gmail.com>
2024-10-14 09:06:23 -0500

    Added features to int_origin_destination_ada to get proportion of riderhsip

--- dbt_fellowship/models/intermediate/_intermediate_docs.md
+++ dbt_fellowship/models/intermediate/_intermediate_docs.md
@@ -18,14 +18,28 @@ Brings all lines in the complex and station names into a list.
 
 Filters origin and destination to all combinations where both `origin_station_complex_id` and
 `destination_station_complex_id` are in `int_ada_complexes.ada_complex_id`. In other words,
-filters the table to all records where both the origin and destination are accessible.
+filters the table to all records where both the origin and destination are accessible. Also
+defines a ridership `proportion_from_origin`. See column-level documentation for more details.
 
 {% enddocs %}
 
 
 {% docs int_hourly_ridership_filteraggregate %}
 
-Aggregates `hourly_ridership` into the same grain as `origin_destination` but instead of *averaging*
-the ridership it totals the ridership. This way, we don't lose information.
+Aggregates `hourly_ridership` into the same grain as `origin_destination` but instead of *averaging* the ridership it totals the ridership. This way, we don't lose information.
+
+{% enddocs %}
+
+{% docs proportion_from_origin %}
+
+Calculates the proportion of riders to the destination given a single composite key of
+(`origin_month`, `origin_day_of_week`, `origin_hour_of_day`, `origin_station_complex_id`). 
+Specifically, let's say for that composite key we summed `estimated_average_ridership` and
+got a column called `total_estimated_average_ridership`, which equaled 100. For example, we
+had 100 riders begin their subway journeys at Union Square on January, Mondays, at midnight. 
+
+Then, we divide each destination by that number to get a *proportion* of riders for that
+destination and composite key. For example, if 20 of the riders above traveled to Grand
+Central, the `proportion_from_origin` would be 0.2 (20%). 
 
 {% enddocs %}
--- dbt_fellowship/models/intermediate/int_origin_destination_ada.sql
+++ dbt_fellowship/models/intermediate/int_origin_destination_ada.sql
@@ -1,7 +1,42 @@
-SELECT 
-	sod.*
-FROM {{ref("stg_origin_destination")}}	                            sod
-inner join	{{ref("int_ada_complexes")}}	                        iac_o
-	on sod.origin_station_complex_id = iac_o.ada_complex_id
-inner join {{ref("int_ada_complexes")}}		                        iac_d
-	on sod.destination_station_complex_id = iac_d.ada_complex_id
+
+WITH origin_destination_ada_CTE AS (
+	SELECT 
+		sod.*
+	FROM {{ref("stg_origin_destination")}}	                            sod
+	inner join	{{ref("int_ada_complexes")}}	                        iac_o
+		on sod.origin_station_complex_id = iac_o.ada_complex_id
+	inner join {{ref("int_ada_complexes")}}		                        iac_d
+		on sod.destination_station_complex_id = iac_d.ada_complex_id
+),
+/*
+ * This cte generates the total ridership for a particular month, day, hour, origin combo.
+ * We can then use this number to find a "proportion" for each destination. That is to say,
+ * if 100 riders on average departed from origin_staiton_complex_id = 622 on January, Monday,
+ * at midnight. And if destination_id = 2 has 20 riders on average, then the total proportion
+ * is 20%. Again, since we are summing averages, this number isn't "reliable" per se. It's just
+ * the best we can do with the available data.
+ */
+total_ridership_CTE AS (
+	SELECT 
+		origin_month
+	   ,origin_day_of_week
+	   ,origin_hour_of_day
+	   ,origin_station_complex_id
+	   ,sum(estimated_average_ridership)	total_estimated_average_ridership	
+	FROM origin_destination_ada_CTE
+	GROUP BY
+		origin_month
+	   ,origin_day_of_week
+	   ,origin_hour_of_day
+	   ,origin_station_complex_id
+)
+
+SELECT
+	od.*
+	,estimated_average_ridership/tr.total_estimated_average_ridership	proportion_from_origin
+FROM origin_destination_ada_CTE				od
+LEFT JOIN total_ridership_CTE   			tr
+	ON 	od.origin_month						 = tr.origin_month
+	AND	od.origin_day_of_week			     = tr.origin_day_of_week
+	AND	od.origin_hour_of_day				 = tr.origin_hour_of_day
+	AND	od.origin_station_complex_id		 = tr.origin_station_complex_id
