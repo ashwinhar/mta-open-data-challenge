ea461a641e2c307f685760bdb5e6da47cd1492e3
Ashwin Hareesh <ashwinhar@gmail.com>
2024-10-21 12:08:10 -0400

    formatting with black

--- src/database_setup/database_setup.py
+++ src/database_setup/database_setup.py
@@ -1,4 +1,5 @@
-'''Module to setup duckdb database with no transformations from raw MTA data'''
+"""Module to setup duckdb database with no transformations from raw MTA data"""
+
 import duckdb
 import pandas as pd
 from sodapy import Socrata
@@ -9,46 +10,44 @@ import os
 load_dotenv()
 
 # Open Data Constsants
-NY_OPEN_DATA_API_TOKEN = os.getenv('NY_OPEN_DATA_API_TOKEN')
-NY_OPEN_DATA_USERNAME = os.getenv('NY_OPEN_DATA_USERNAME')
-NY_OPEN_DATA_PASSWORD = os.getenv('NY_OPEN_DATA_PASSWORD')
+NY_OPEN_DATA_API_TOKEN = os.getenv("NY_OPEN_DATA_API_TOKEN")
+NY_OPEN_DATA_USERNAME = os.getenv("NY_OPEN_DATA_USERNAME")
+NY_OPEN_DATA_PASSWORD = os.getenv("NY_OPEN_DATA_PASSWORD")
 
 # Database Constants
-MTA_SCHEMA = os.getenv('MTA_SCHEMA')
-DEV_DATABASE = os.getenv('DEV_DATABASE')
-MANUAL_ENTRY_SCHEMA = os.getenv('MANUAL_ENTRY_SCHEMA')
+MTA_SCHEMA = os.getenv("MTA_SCHEMA")
+DEV_DATABASE = os.getenv("DEV_DATABASE")
+MANUAL_ENTRY_SCHEMA = os.getenv("MANUAL_ENTRY_SCHEMA")
 
 # File paths
-NEAREST_ADA_STATIONS_PATH = 'data/nearest_ada_stations.csv'
-TRAVEL_TIMES_PATH = 'data/travel_times.csv'
+NEAREST_ADA_STATIONS_PATH = "data/nearest_ada_stations.csv"
+TRAVEL_TIMES_PATH = "data/travel_times.csv"
 #############################################################################################
 
 
-def extract_dataset(dataset_code: str,
-                    where_clause: str = None
-                    ) -> pd.DataFrame:
+def extract_dataset(dataset_code: str, where_clause: str = None) -> pd.DataFrame:
     """
     Extract dataset via API connection to NY Open Data Program
 
     Parameters:
         dataset_code     - A dataset code from the NY Open Data Program. Examples can be found in mta_dataset.py
-        where_clause     - API calls can be restricted. Defines the filter. 
+        where_clause     - API calls can be restricted. Defines the filter.
 
-    Returns: 
-        pd.Dataframe - Results returned from the API call 
+    Returns:
+        pd.Dataframe - Results returned from the API call
     """
 
     limit = 100000000  # Limit records on API call
 
-    client = Socrata("data.ny.gov",
-                     NY_OPEN_DATA_API_TOKEN,
-                     username=NY_OPEN_DATA_USERNAME,
-                     password=NY_OPEN_DATA_PASSWORD)
+    client = Socrata(
+        "data.ny.gov",
+        NY_OPEN_DATA_API_TOKEN,
+        username=NY_OPEN_DATA_USERNAME,
+        password=NY_OPEN_DATA_PASSWORD,
+    )
 
     if where_clause:
-        results = client.get(dataset_code,
-                             where=where_clause,
-                             limit=limit)
+        results = client.get(dataset_code, where=where_clause, limit=limit)
     else:
         results = client.get(dataset_code, limit=limit)
 
@@ -60,21 +59,20 @@ def get_database_connection(database_path: str) -> duckdb.DuckDBPyConnection:
     Return connection to local DuckDB database
 
     Params:
-        database_path - Absolute path 
-    Returns: 
-        DuckDBPyConnection 
+        database_path - Absolute path
+    Returns:
+        DuckDBPyConnection
     """
     return duckdb.connect(database_path)
 
 
-def create_table(db_conn: duckdb.DuckDBPyConnection,
-                 results_dataframe: pd.DataFrame,
-                 table_name: str
-                 ) -> None:
+def create_table(
+    db_conn: duckdb.DuckDBPyConnection, results_dataframe: pd.DataFrame, table_name: str
+) -> None:
     """
     Create table in local DuckDB database
 
-    Params: 
+    Params:
         db_conn - Connection to local DuckDB database. Typically returned by get_database_connection()
         results_dataframe - Dataframe to create. Typically returned by extract_dataset()
         table_name - Full database object identifier
@@ -85,13 +83,11 @@ def create_table(db_conn: duckdb.DuckDBPyConnection,
     db_conn.sql(statement)
 
 
-def drop_table(db_conn: duckdb.DuckDBPyConnection,
-               identifier: str
-               ) -> None:
+def drop_table(db_conn: duckdb.DuckDBPyConnection, identifier: str) -> None:
     """
     Drop table if exists in local duckdb file
 
-    Params: 
+    Params:
         db_conn - Connection to local DuckDB database. Typically returned by get_database_connection()
         identifier - Database object to drop
 
@@ -100,13 +96,11 @@ def drop_table(db_conn: duckdb.DuckDBPyConnection,
     db_conn.sql(statement)
 
 
-def create_schema(db_conn: duckdb.DuckDBPyConnection,
-                  schema_name: str
-                  ) -> None:
+def create_schema(db_conn: duckdb.DuckDBPyConnection, schema_name: str) -> None:
     """
     Create schema in database if not exists
 
-    Params: 
+    Params:
         db_conn - Connection to local DuckDB database. Typically returned by get_database_connection()
         schema_name - Schema name as string
     """
@@ -116,7 +110,7 @@ def create_schema(db_conn: duckdb.DuckDBPyConnection,
 
 def table_exists(db_conn: duckdb.DuckDBPyConnection, schema_name, table_name) -> bool:
     """
-    Checks if table exists in DuckDB database. 
+    Checks if table exists in DuckDB database.
 
     Params:
         db_conn - Connection to local DuckDB database. Typically returned by get_database_connection()
@@ -137,7 +131,9 @@ def table_exists(db_conn: duckdb.DuckDBPyConnection, schema_name, table_name) ->
     return result is not None
 
 
-def new_create_table(conn: duckdb.DuckDBPyConnection, mta_dataset: mta.MTADataset, overwrite=False) -> None:
+def new_create_table(
+    conn: duckdb.DuckDBPyConnection, mta_dataset: mta.MTADataset, overwrite=False
+) -> None:
     """
     Full EL process from NY Open Dataset to local database
 
@@ -147,14 +143,15 @@ def new_create_table(conn: duckdb.DuckDBPyConnection, mta_dataset: mta.MTADatase
         overwrite (bool): If True, then database object at mta_dataset.identifier is dropped and created fresh
 
     """
-    table_exists_flag = table_exists(
-        conn, mta_dataset.schema, mta_dataset.table_name)
+    table_exists_flag = table_exists(conn, mta_dataset.schema, mta_dataset.table_name)
 
     if not table_exists_flag or overwrite:
         try:
             print(f"Starting extract for {mta_dataset.table_name}")
             df = extract_dataset(
-                mta_dataset.code, mta_dataset.default_where_clause, )
+                mta_dataset.code,
+                mta_dataset.default_where_clause,
+            )
             print("Extract successful")
             print("Creating table in database")
             drop_table(conn, mta_dataset.identifier)
@@ -193,15 +190,24 @@ def default_setup(overwrite=False) -> None:
         # Create manual entry nearest_ada_stations table. Manual entry tables have a different flow.
         # Note that nearest_ada_stations was built by hand, not via API request
         nearest_ada_stations_df = pd.read_csv(NEAREST_ADA_STATIONS_PATH)
-        if not table_exists(conn, MANUAL_ENTRY_SCHEMA, 'nearest_ada_stations') or overwrite:
-            create_table(conn, nearest_ada_stations_df, f'{
-                         MANUAL_ENTRY_SCHEMA}.nearest_ada_stations')
+        if (
+            not table_exists(conn, MANUAL_ENTRY_SCHEMA, "nearest_ada_stations")
+            or overwrite
+        ):
+            create_table(
+                conn,
+                nearest_ada_stations_df,
+                f"{MANUAL_ENTRY_SCHEMA}.nearest_ada_stations",
+            )
 
         # Create manual entry travel_times table
         travel_times_df = pd.read_csv(TRAVEL_TIMES_PATH)
-        if not table_exists(conn, MANUAL_ENTRY_SCHEMA, 'travel_times') or overwrite:
-            create_table(conn, travel_times_df, f'{
-                         MANUAL_ENTRY_SCHEMA}.travel_times')
+        if not table_exists(conn, MANUAL_ENTRY_SCHEMA, "travel_times") or overwrite:
+            create_table(
+                conn,
+                travel_times_df,
+                f"{MANUAL_ENTRY_SCHEMA}.travel_times",
+            )
 
 
 if __name__ == "__main__":
