56af20fa80048e74f6b765830323e3261ac853e1
Ashwin Hareesh <ashwinhar@gmail.com>
2024-10-22 10:45:01 -0400

    making union_staging macro more modular

--- src/accessibility_dbt/macros/union_staging.sql
+++ src/accessibility_dbt/macros/union_staging.sql
@@ -1,11 +1,11 @@
-{% macro union_tables(table_name, num_tables) %}
+{% macro union_tables(table_name, columns_macro, num_tables) %}
     {% set unioned_query = [] %}
 
     {% for i in range(1, num_tables + 1) %}
         {% set source_name = (table_name) ~ i %}
         {% set query_part = (
             "SELECT "
-            ~ origin_destination_columns()
+            ~ columns_macro
             ~ " FROM "
             ~ source("mta", (source_name))
         ) %}
