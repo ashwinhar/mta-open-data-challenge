d467d15f34e3af56cdc8ec7e0c6c64094bb63721
Ashwin Hareesh <ashwinhar@gmail.com>
2024-10-23 09:00:36 -0400

    more verbose fct_time_loss

--- src/accessibility_dbt/models/marts/fct_time_loss.sql
+++ src/accessibility_dbt/models/marts/fct_time_loss.sql
@@ -1,31 +1,38 @@
 with
-    granular_cte as (
+    -- Find total estimated ada ridership for each *existing* ada-compliant complex 
+    ridership_per_destination_cte as (
         select
             destination_station_complex_id as existing_ada_complex_id,
-            -- Multiplying the estimated amount of ada rides per the corresponding
-            -- "travel time difference" between a train ride and walking. Dividing by
-            -- 3600 converts from seconds to hours
-            round(
-                (sum(re.estimated_ada_ridership * tta.avg_travel_time_diff)) / 3600, 2
-            ) as total_time_loss_hrs
+            sum(re.estimated_ada_ridership) as estimated_ada_ridership
         from {{ ref("int_ridership_estimation") }} re
-        inner join
-            {{ ref("int_travel_times_aggregated") }} tta
-            on re.destination_station_complex_id = tta.existing_ada_complex_id
         group by re.destination_station_complex_id
+    ),
+    -- Find the total time loss for each relevant existing ada complex
+    calculation_before_scalars_cte as (
+        select
+            rpd.existing_ada_complex_id,
+            iac.stop_name_list as existing_ada_stop_names,
+            tta.list_closest_planned_stations,
+            rpd.estimated_ada_ridership,
+            tta.avg_travel_time_diff,
+            round(
+                rpd.estimated_ada_ridership * tta.avg_travel_time_diff / 3600, 2
+            ) as total_time_loss_hrs
+        from ridership_per_destination_cte as rpd
+        left join
+            {{ ref("int_ada_complexes") }} as iac
+            on rpd.existing_ada_complex_id = iac.ada_complex_id
+        inner join
+            {{ ref("int_travel_times_aggregated") }} as tta
+            on tta.existing_ada_complex_id = rpd.existing_ada_complex_id
     )
 select
-    gc.existing_ada_complex_id,
-    iac.stop_name_list as existing_ada_stop_names,
-    tta.list_closest_planned_stations,
-    -- Apply estimation multipliers (assume 10% of rides had different intended
-    -- destination, 20%, etc)
-    {{ apply_multipliers("total_time_loss_hrs") }}
-from granular_cte as gc
-left join
-    {{ ref("int_ada_complexes") }} as iac
-    on gc.existing_ada_complex_id = iac.ada_complex_id
-left join
-    {{ ref("int_travel_times_aggregated") }} as tta
-    on gc.existing_ada_complex_id = tta.existing_ada_complex_id
-order by gc.existing_ada_complex_id asc
+    existing_ada_complex_id,
+    list_extract(existing_ada_stop_names, 1) as existing_ada_complex_alias,
+    len(list_closest_planned_stations) as num_planned_ada_stations,
+    list_closest_planned_stations as planned_ada_stations,
+    round(estimated_ada_ridership, 0) as estimated_ada_ridership,
+    avg_travel_time_diff as avg_travel_time_diff_sec,
+    {{ apply_multipliers("total_time_loss_hrs") }},
+    total_time_loss_hrs as total_time_loss_hrs_100p
+from calculation_before_scalars_cte as cbs
--- src/accessibility_dbt/tests/nearest_stations_quality_check.sql
+++ src/accessibility_dbt/tests/nearest_stations_quality_check.sql
@@ -1,9 +1,11 @@
-SELECT
-	*
-FROM manual_entry.nearest_ada_stations 				nas 
-left join mta.stations 								s 
-	on  nas.planned_ada_station_id  = s.station_id 
-	and nas.planned_ada_complex_id 	= s.complex_id 
-where
-		nas.planned_ada_complex_id is null
-	or	nas.planned_ada_station_id is null
+-- Test name: nearest_stations_quality_check
+-- Description: Ensure that all complex_id and station_id values in the
+-- manual_entry.nearest_stations table actually exist in the MTA dataset.
+-- Severity: error
+select *
+from manual_entry.nearest_ada_stations nas
+left join
+    mta.stations s
+    on nas.planned_ada_station_id = s.station_id
+    and nas.planned_ada_complex_id = s.complex_id
+where nas.planned_ada_complex_id is null or nas.planned_ada_station_id is null
