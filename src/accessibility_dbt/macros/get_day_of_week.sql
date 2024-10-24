ede4034cea4457b99129be9e79f50d4001e3c33f
Ashwin Hareesh <ashwinhar@gmail.com>
2024-10-24 16:36:39 -0400

    bug fix

--- src/accessibility_dbt/macros/get_day_of_week.sql
+++ src/accessibility_dbt/macros/get_day_of_week.sql
@@ -2,19 +2,19 @@
     -- This macro returns the name of the day (Monday, Tuesday, etc.)
     -- based on the 'dayofweek' extracted from the given timestamp.
     case
-        when datepart('dayofweek', {{ timestamp }}) = 1
+        when datepart('dayofweek', {{ timestamp }}) = 0
         then 'Monday'
-        when datepart('dayofweek', {{ timestamp }}) = 2
+        when datepart('dayofweek', {{ timestamp }}) = 1
         then 'Tuesday'
-        when datepart('dayofweek', {{ timestamp }}) = 3
+        when datepart('dayofweek', {{ timestamp }}) = 2
         then 'Wednesday'
-        when datepart('dayofweek', {{ timestamp }}) = 4
+        when datepart('dayofweek', {{ timestamp }}) = 3
         then 'Thursday'
-        when datepart('dayofweek', {{ timestamp }}) = 5
+        when datepart('dayofweek', {{ timestamp }}) = 4
         then 'Friday'
-        when datepart('dayofweek', {{ timestamp }}) = 6
+        when datepart('dayofweek', {{ timestamp }}) = 5
         then 'Saturday'
-        when datepart('dayofweek', {{ timestamp }}) = 7
+        when datepart('dayofweek', {{ timestamp }}) = 6
         then 'Sunday'
     end
 {% endmacro %}
